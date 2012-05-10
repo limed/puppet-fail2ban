58b84a5d31905da030e0d6f86d2d9a7484e44443
limed <edward.lim@oregonstate.edu>
2012-05-10 11:30:27 -0700

    Rewrite of fail2ban module

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,52 +1,64 @@
-# main fail2ban class
-#
-# Usage:
-#   include fail2ban
 
-class fail2ban {
+class fail2ban(
+    $ensure     = present,
+    $bantime    = '1200',
+    $ignore     = ['*.$::domain', '127.0.0.1'],
+    $maxretry   = '3',
+    $email      = "root@${::domain}") {
 
-    require fail2ban::params
+    include fail2ban::params
 
-    # Reset variables needed in templates
-    $fail2ban_ignore    = $fail2ban::params::ignore
-    $fail2ban_bantime   = $fail2ban::params::bantime
-    $fail2ban_maxretry  = $fail2ban::params::maxretry
-    $fail2ban_destemail = $fail2ban::params::destemail
-
-    package { "fail2ban":
-        name    => "${fail2ban::params::packagename}",
-        ensure  => present
+    if ! ($ensure in ['present','absent']) {
+        fail('fail2ban module requires present or absent')
     }
 
-    service { "fail2ban":
-        ensure      => running,
-        name        => "${fail2ban::params::servicename}",
-        enable      => true,
+    if $ensure == present {
+        $package_ensure = present
+        $service_ensure = running
+        $service_enable = true
+    }
+    else {
+        $package_ensure = absent
+        $service_ensure = stopped
+        $service_enable = false
+    }
+
+    validate_array($ignore)
+
+    package { 'fail2ban':
+        ensure  => $package_ensure,
+        name    => $fail2ban::params::packagename,
+    }
+
+    service { 'fail2ban':
+        ensure      => $service_ensure,
+        name        => $fail2ban::params::servicename,
+        enable      => $service_enable,
         hasstatus   => true,
         hasrestart  => true,
-        pattern     => "${fail2ban::params::processname}",
-        require     => Package["fail2ban"],
+        pattern     => $fail2ban::params::processname,
+        require     => Package['fail2ban'],
     }
 
-    file { "jail.conf":
+    file { 'jail.conf':
         ensure  => present,
-        path    => "${fail2ban::params::configfile}",
+        path    => $fail2ban::params::configfile,
         owner   => root,
         group   => root,
         mode    => '0644',
-        require => Package["fail2ban"],
-        notify  => Service["fail2ban"],
+        require => Package['fail2ban'],
+        notify  => Service['fail2ban'],
     }
 
-    file { "jail.local":
+    file { 'jail.local':
         ensure  => present,
         path    => "${fail2ban::params::configdir}/jail.local",
         owner   => root,
         group   => root,
         mode    => '0644',
-        require => Package["fail2ban"],
-        content => template("fail2ban/jail.local.erb"),
-        notify  => Service["fail2ban"],
+        require => Package['fail2ban'],
+        content => template('fail2ban/jail.local.erb'),
+        notify  => Service['fail2ban'],
     }
 
 }
--- manifests/params.pp
+++ manifests/params.pp
@@ -1,46 +1,11 @@
-# fail2ban::params class - we set our variables here
-#
 
 class fail2ban::params {
 
-    $packagename = $operatingsystem ? {
-        default => "fail2ban",
-    }
-
-    $servicename = $operatingsystem ? {
-        default => "fail2ban",
-    }
-
-    $processname = $operatingsystem ? {
-        default => "fail2ban-server",
-    }
-
-    $configdir  = $operatingsystem ? {
-        default => "/etc/fail2ban",
-    }
-
-    $configfile = $operatingsystem ? {
-        default => "/etc/fail2ban/jail.conf",
-    }
-
-    $ignore = $fail2ban_ignore ? {
-        ''      => [ "*.$domain", "127.0.0.1" ],
-        default => $fail2ban_ignore,
-    }
-
-    $bantime = $fail2ban_bantime ? {
-        ''      => "1200",
-        default => $fail2ban_bantime
-    }
-
-    $maxretry = $fail2ban_maxretry ? {
-        ''      => "3",
-        default => $fail2ban_maxretry
-    }
-
-    $destemail = $fail2ban_destemail ? {
-        ''      => "root@$domain",
-        default => $fail2ban_destemail,
-    }
+    $packagename    = 'fail2ban'
+    $servicename    = 'fail2ban'
+    $servicepattern = '/usr/bin/python /usr/bin/fail2ban-server'
+    $processname    = 'fail2ban-server'
+    $configdir      = '/etc/fail2ban'
+    $configfile     = '/etc/fail2ban/jail.conf'
 
 }
